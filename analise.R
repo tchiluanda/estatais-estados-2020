fc4a6afa75a8f407466c18bfc82c83598e703710
tiagombp <tiagombp@gmail.com>
2020-10-15 18:34:44 -0300

    cria função para fazer alterações pontuais na base

--- analise.R
+++ analise.R
@@ -3,7 +3,8 @@
 
 library(tidyverse)
 library(readxl)
-
+library(geobr)
+library(viridis)
 
 
 # dados iniciais ----------------------------------------------------------
@@ -137,6 +138,7 @@ limpa_setor = data.frame(
 linha_CMTP <- dados_selecionados_raw$emp == "CMTP"
 dados_selecionados_raw[linha_CMTP, "PL"] <- as.character(20.2e6)
 dados_selecionados_raw[linha_CMTP, "lucros"] <- as.character(236.8e3)
+dados_selecionados_raw[linha_CMTP, "maior_rem"] <- as.character(9.4e3)
 
 # junta todo mundo
 
@@ -149,4 +151,50 @@ dados_selecionados <- dados_selecionados_raw %>%
     PL     = as.numeric(PL),
     lucros = as.numeric(lucros))
 
-rep <- dados_selecionados %>% count(emp)
+#verifica empresas repetidas
+#rep <- dados_selecionados %>% count(emp)
+
+# corrige na mão alguns setores
+
+termos <- c("COMPESA", "SUAPE", "DOCAS", "PORTOS", "Portos", "CAEMA")
+
+gera_vetor <- function(termo){
+  return(str_detect(dados_selecionados$emp, termo))
+}
+
+linhas <- map(termos, gera_vetor)
+names(linhas) <- termos
+
+atribui <- function(termo, coluna, valor){
+  #print(dados_selecionados[linhas[[termo]], coluna])
+  # pulo do gato aqui é o <<- para fazer o assignment na variável global
+  dados_selecionados[linhas[[termo]], coluna] <<- valor
+  #print(dados_selecionados[linhas[[termo]], coluna])
+}
+
+atribui("COMPESA", "setor", "SANEAMENTO")
+atribui("SUAPE", "setor", "PORTOS E HIDROVIAS")
+atribui("DOCAS", "setor", "PORTOS E HIDROVIAS")
+atribui("PORTOS", "setor", "PORTOS E HIDROVIAS")
+atribui("Portos", "setor", "PORTOS E HIDROVIAS")
+atribui("CAEMA", "setor", "SANEAMENTO")
+atribui("COMPESA", "setor", "SANEAMENTO")
+
+#dados_selecionados[linhas[["CAEMA"]], "setor"] <- "SANEAMENTO"
+#dados_selecionados[linhas[["COMPESA"]], "setor"]
+
+# gráficos anteriores -- mapas --------------------------------------------
+
+dados_qde_setor_estado <- dados_selecionados %>%
+  count(setor, Estado)
+
+mapa <- geobr::read_state()
+
+mapa_qde <- mapa %>%
+  inner_join(dados_qde_setor_estado, by = c("abbrev_state" = "Estado"))
+
+ggplot(mapa_qde %>% filter(setor == "SANEAMENTO")) + 
+    geom_sf(aes(fill = n > 0), color = "coral") +
+    scale_fill_manual(values = c("TRUE" = "lightcoral", "FALSE" = NA)) +
+    labs(fill = "Tem empresa de saneamento?")
+
